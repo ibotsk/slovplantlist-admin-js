55855bee6a81f59f37f0f20967ce4c29f7950a38
Matus Kempa <matus.kempa@gmail.com>
2018-12-01 20:05:53 +0100

    Using HOC and composition in pages

--- src/components/pages/Checklist.js
+++ src/components/pages/Checklist.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react';
-import { Grid, Well } from 'react-bootstrap';
-import axios from 'axios';
-import template from 'url-template';
+import React from 'react';
 
-import Filter from '../segments/Filter';
-import CPaginator from '../segments/CPaginator';
-import CTable from '../segments/CTable';
 import LosName from '../segments/LosName';
+import TabledPage from './TabledPageParent';
 
 import config from '../../config/config';
 
@@ -14,94 +9,34 @@ const PAGE_DETAIL = "/checklist/detail/";
 const searchFields = ["genus", "species", "genus", "species", "subsp", "var", "subvar", "forma",
     "nothosubsp", "nothoforma", "authors", "genus_h", "species_h", "subsp_h", "var_h", "subvar_h", "forma_h",
     "nothosubsp_h", "nothoforma_h", "authors_h", "publication", "tribus", "vernacular"];
+const tableHeader = ["ID", "Type", "Name", "Publication", "Accepted name"];
 
-class Checklist extends Component {
-
-    getAllUri = template.parse(config.uris.nomenclaturesUri.getAll);
-    getCountUri = template.parse(config.uris.nomenclaturesUri.count);
-
-    state = {
-        nomenclature: [],
-        numOfRecords: 0,
-        activePage: 1,
-        where: {}
-    };
-
-    handlePageChange(activePage) {
-        this.handleChange(activePage, this.state.where);
-        this.setState({ activePage: activePage });
-    }
-
-    handleFilterChange(where) {
-        this.handleChange(this.state.activePage, where);
-        this.setState({ where: where });
-    }
-
-    handleChange(activePage, where) {
-        return this.fetchCount(where).then(() => {
-            const page = Math.max(activePage - 1, 0);
-            const limit = config.format.recordsPerPage;
-            const offset = page * limit;
-            return this.fetchRecords(where, offset, limit);
-        }).then(response => {
-            const noms = this.formatResult(response);
-            this.setState({ nomenclature: noms });
-        }).catch(e => console.error(e));
-    }
-
-    formatResult(result) {
-        return result.data.map(d => {
-            return {
-                id: d.id,
-                type: d.ntype,
-                name: <a href={`${PAGE_DETAIL}${d.id}`} ><LosName key={d.id} nomen={d} format='plain' /></a>,
-                publication: d.publication,
-                acceptedName: <a href={d.accepted ? `${PAGE_DETAIL}${d.accepted.id}` : ""}><LosName key={`acc${d.id}`} nomen={d.accepted} format='plain' /></a>
-            }
-        });
-    }
-
-    fetchRecords(where, offset, limit) {
-        const uri = this.getAllUri.expand({ offset: offset, where: JSON.stringify(where), limit: limit });
-        return axios.get(uri);
-    }
-
-    fetchCount(where) {
-        const whereString = JSON.stringify(where);
-        const uri = this.getCountUri.expand({ base: config.uris.backendBase, whereString: whereString });
-        return axios.get(uri).then(response => this.setState({ numOfRecords: response.data.count }));
-    }
-
-    componentDidMount() {
-        this.handleChange(this.state.activePage, this.state.where);
-    }
-
-    render() {
-        const header = ["ID", "Type", "Name", "Publication", "Accepted name"];
-        return (
-            <div id="checklist">
-                <Grid id="functions">
-                    <Well>
-                        <Filter
-                            onHandleChange={(where) => this.handleFilterChange(where)}
-                            searchFields={searchFields}
-                            searchFieldMinLength={config.format.searchFieldMinLength}
-                        />
-                    </Well>
-                </Grid>
-                <Grid fluid={true}>
-                    <CPaginator
-                        totalItems={this.state.numOfRecords}
-                        recordsPerPage={config.format.recordsPerPage}
-                        displayRange={config.format.rangeDisplayed}
-                        numOfElementsAtEnds={config.format.numOfElementsAtEnds}
-                        onHandleSelect={(activePage) => this.handlePageChange(activePage)}
-                    />
-                    <CTable head={header} rows={this.state.nomenclature} />
-                </Grid>
-            </div>
-        );
-    }
+const formatResult = (result) => {
+    return result.data.map(d => {
+        return {
+            id: d.id,
+            type: d.ntype,
+            name: <a href={`${PAGE_DETAIL}${d.id}`} ><LosName key={d.id} nomen={d} format='plain' /></a>,
+            publication: d.publication,
+            acceptedName: <a href={d.accepted ? `${PAGE_DETAIL}${d.accepted.id}` : ""}><LosName key={`acc${d.id}`} nomen={d.accepted} format='plain' /></a>
+        }
+    });
 }
 
-export default Checklist;
+const Checklist = (props) => {
+
+    return (
+        <div id='checklist'>
+            {props.children}
+        </div>
+    )
+
+}
+
+export default TabledPage({ 
+    getAll: config.uris.nomenclaturesUri.getAll, 
+    getCount: config.uris.nomenclaturesUri.count, 
+    tableHeader,
+    searchFields,
+    formatResult 
+})(Checklist);
--- src/components/pages/Genera.js
+++ src/components/pages/Genera.js
@@ -1,88 +1,39 @@
-import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
-import axios from 'axios';
-import template from 'url-template';
+import React from 'react';
 
-import CPaginator from '../segments/CPaginator';
-import CTable from '../segments/CTable';
+import TabledPage from './TabledPageParent';
 
 import config from '../../config/config';
 
-class Genera extends Component {
+const tableHeader = ["ID", "Name", "Authors", "Vernacular", "Family APG", "Family"];
+const searchFields = ["name", "authors", "vernacular"];
 
-    getAllUri = template.parse(config.uris.generaUri.getAll);
-    getCountUri = template.parse(config.uris.generaUri.count);
+const Genera = (props) => {
 
-    state = {
-        records: [],
-        numOfRecords: 0,
-        activePage: 1,
-        where: {}
-    };
-
-    handlePageChange(activePage) {
-        this.handleChange(activePage, this.state.where);
-        this.setState({ activePage: activePage });
-    }
-
-    handleChange(activePage, where) {
-        return this.fetchCount(where).then(() => {
-            const page = Math.max(activePage - 1, 0);
-            const limit = config.format.recordsPerPage;
-            const offset = page * limit;
-            return this.fetchRecords(where, offset, limit);
-        }).then(response => {
-            const noms = this.formatResult(response);
-            this.setState({ records: noms });
-        }).catch(e => console.error(e));
-    }
-
-    formatResult(result) {
-        return result.data.map(d => {
-            return {
-                id: d.id,
-                name: d.name,
-                authors: d.authors,
-                vernacular: d.vernacular,
-                familyAPG: "",
-                family: ""
-            }
-        });
-    }
-
-    fetchRecords(where, offset, limit) {
-        const uri = this.getAllUri.expand({ offset: offset, where: JSON.stringify(where), limit: limit});
-        return axios.get(uri);
-    }
-
-    fetchCount(where) {
-        const whereString = JSON.stringify(where);
-        const uri = this.getCountUri.expand({ base: config.uris.backendBase, whereString: whereString });
-        return axios.get(uri).then(response => this.setState({ numOfRecords: response.data.count }));
-    }
-
-    componentDidMount() {
-        this.handleChange(this.state.activePage, this.state.where);
-    }
-
-    render() {
-        const header = ["ID", "Name", "Authors", "Vernacular", "Family APG", "Family"];
-        return (
-            <div id="checklist">
-                <Grid fluid={true}>
-                    <CPaginator
-                        totalItems={this.state.numOfRecords}
-                        recordsPerPage={config.format.recordsPerPage}
-                        displayRange={config.format.rangeDisplayed}
-                        numOfElementsAtEnds={config.format.numOfElementsAtEnds}
-                        onHandleSelect={(activePage) => this.handlePageChange(activePage)}
-                    />
-                    <CTable head={header} rows={this.state.records} />
-                </Grid>
-            </div>
-        );
-    }
+    return (
+        <div id='genera'>
+            {props.children}
+        </div>
+    )
 
 }
 
-export default Genera;
+const formatResult = (result) => {
+    return result.data.map(d => {
+        return {
+            id: d.id,
+            name: d.name,
+            authors: d.authors,
+            vernacular: d.vernacular,
+            familyAPG: "",
+            family: ""
+        }
+    });
+}
+
+export default TabledPage({ 
+    getAll: config.uris.generaUri.getAll, 
+    getCount: config.uris.generaUri.count, 
+    tableHeader,
+    searchFields,
+    formatResult 
+})(Genera);
